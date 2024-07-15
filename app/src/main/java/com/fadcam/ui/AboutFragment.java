5a9cd3f4cc5a822f8b464d02db1f50140486706a
Faded <anonfaded@pm.me>
2024-07-15 06:26:44 +0500

    about fragment completed fully

--- app/src/main/java/com/fadcam/ui/AboutFragment.java
+++ app/src/main/java/com/fadcam/ui/AboutFragment.java
@@ -1,6 +1,9 @@
 package com.fadcam.ui;
 
+
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -27,19 +30,34 @@ public class AboutFragment extends Fragment {
         View donateButton = view.findViewById(R.id.donate_button);
         TextView emailText = view.findViewById(R.id.email_text);
         TextView discordText = view.findViewById(R.id.discord_text);
+        TextView privacyInfoText = view.findViewById(R.id.privacy_info_text);
 
         appIcon.setImageResource(R.mipmap.ic_launcher);
         appName.setText(getString(R.string.app_name));
-        Object BuildConfig = null;
-        appVersion.setText(String.format("Version %s", BuildConfig));
-        appDescription.setText(getString(R.string.app_description));
+        appVersion.setText(String.format("Version %s", getAppVersion()));
+//        appDescription.setText(getString(R.string.app_description));
 
         sourceCodeButton.setOnClickListener(v -> openUrl("https://github.com/anonfaded/FadCam"));
         donateButton.setOnClickListener(v -> openUrl("https://ko-fi.com/fadedx"));
         emailText.setOnClickListener(v -> sendEmail());
         discordText.setOnClickListener(v -> openUrl("https://discord.gg/gUMnBJxGBW"));
+        String privacyInfo = "Q: Does FadCam collect any user data?\nA: No, FadCam does not collect, track, or analyze any user data. Your privacy is our priority.\n\n" +
+                "Q: Are there any ads in FadCam?\nA: FadCam is completely ad-free, providing an uninterrupted user experience.";
+        privacyInfoText.setText(privacyInfo);
+
 
         return view;
+    }
+
+    private String getAppVersion() {
+        try {
+            PackageManager pm = getActivity().getPackageManager();
+            PackageInfo pInfo = pm.getPackageInfo(getActivity().getPackageName(), 0);
+            return pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "N/A";
+        }
     }
 
     private void openUrl(String url) {
