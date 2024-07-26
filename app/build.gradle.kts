4da6e769d77312147cdbd70c48a5dd308754bd0e
Faded <anonfaded@pm.me>
2024-07-26 05:36:49 +0500

    removed cameraX dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,21 +51,5 @@ dependencies {
     implementation(libs.exoplayer.ui)
     implementation(libs.okhttp)
     implementation(libs.viewpager2)
-    implementation(libs.camera.core)
-    // CameraX core library using the camera2 implementation
-    val camerax_version = "1.3.0-beta01"
-    // The following line is optional, as the core library is included indirectly by camera-camera2
-    implementation("androidx.camera:camera-core:${camerax_version}")
-    implementation("androidx.camera:camera-camera2:${camerax_version}")
-    // If you want to additionally use the CameraX Lifecycle library
-    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
-    // If you want to additionally use the CameraX VideoCapture library
-    implementation("androidx.camera:camera-video:${camerax_version}")
-    // If you want to additionally use the CameraX View class
-    implementation("androidx.camera:camera-view:${camerax_version}")
-    // If you want to additionally add CameraX ML Kit Vision Integration
-    implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
-    // If you want to additionally use the CameraX Extensions library
-    implementation("androidx.camera:camera-extensions:${camerax_version}")
 
 }
