5f46d89d41b0f799324fabec67e0aaf18fd92623
Faded <anonfaded@pm.me>
2024-08-07 18:18:12 +0500

    fixed: replaced download apk internal option to instead redirect to F-Droid or github, added review option v1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.fadcam"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
