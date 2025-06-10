d39f39c551b66745b5f8803edbd4145b4ca7c002
Faded <anonfaded@pm.me>
2025-06-10 15:46:06 +0500

    feat: add detailed logging for fingerprint authentication process in FingerprintLockService

--- app/libs/AppLockLibrary/src/main/java/com/guardanis/applock/services/FingerprintLockService.java
+++ app/libs/AppLockLibrary/src/main/java/com/guardanis/applock/services/FingerprintLockService.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.util.Log;
 
 import com.guardanis.applock.AppLock;
 import com.fadcam.R;
@@ -17,6 +18,8 @@ import androidx.core.hardware.fingerprint.FingerprintManagerCompat;
 import androidx.core.os.CancellationSignal;
 
 public class FingerprintLockService extends LockService {
+
+    private static final String TAG = "FingerprintLockService";
 
     public interface AuthenticationDelegate {
         public void onResolutionRequired(int errorCode);
@@ -32,9 +35,11 @@ public class FingerprintLockService extends LockService {
 
     @Override
     public boolean isEnrollmentEligible(Context context) {
-        return Build.VERSION_CODES.M <= Build.VERSION.SDK_INT
+        boolean eligible = Build.VERSION_CODES.M <= Build.VERSION.SDK_INT
                 && context.getResources().getBoolean(R.bool.applock__fingerprint_service_enabled)
                 && isHardwarePresent(context);
+        Log.d(TAG, "isEnrollmentEligible: " + eligible + ", SDK: " + Build.VERSION.SDK_INT + ", hardware: " + isHardwarePresent(context));
+        return eligible;
     }
 
     public void enroll(Context context, AuthenticationDelegate delegate) {
@@ -66,72 +71,74 @@ public class FingerprintLockService extends LockService {
      */
     protected int getRequiredResolutionErrorCode(Context context, boolean localEnrollmentRequired) {
         FingerprintManagerCompat manager = FingerprintManagerCompat.from(context);
-
-        if (Build.VERSION.SDK_INT <  Build.VERSION_CODES.M)
+        Log.d(TAG, "Checking fingerprint requirements. SDK: " + Build.VERSION.SDK_INT);
+        if (Build.VERSION.SDK_INT <  Build.VERSION_CODES.M) {
+            Log.d(TAG, "SDK version too low for fingerprint");
             return AppLock.ERROR_CODE_SDK_VERSION_MINIMUM;
-
-        if (localEnrollmentRequired && !isEnrolled(context))
+        }
+        if (localEnrollmentRequired && !isEnrolled(context)) {
+            Log.d(TAG, "Not locally enrolled");
             return AppLock.ERROR_CODE_FINGERPRINTS_NOT_LOCALLY_ENROLLED;
-
-        if (!isHardwarePresent(context))
+        }
+        if (!isHardwarePresent(context)) {
+            Log.d(TAG, "Fingerprint hardware not present");
             return AppLock.ERROR_CODE_FINGERPRINTS_MISSING_HARDWARE;
-
-        if (!manager.hasEnrolledFingerprints())
+        }
+        if (!manager.hasEnrolledFingerprints()) {
+            Log.d(TAG, "No fingerprints enrolled in system");
             return AppLock.ERROR_CODE_FINGERPRINTS_EMPTY;
-
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED)
+        }
+        int permFingerprint = ContextCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT);
+        int permBiometric = ContextCompat.checkSelfPermission(context, Manifest.permission.USE_BIOMETRIC);
+        Log.d(TAG, "USE_FINGERPRINT permission: " + permFingerprint + ", USE_BIOMETRIC: " + permBiometric);
+        if (permFingerprint != PackageManager.PERMISSION_GRANTED && permBiometric != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Neither USE_FINGERPRINT nor USE_BIOMETRIC permission granted");
             return AppLock.ERROR_CODE_FINGERPRINTS_PERMISSION_REQUIRED;
-
+        }
         return -1;
     }
 
     protected void attemptFingerprintManagerAuthentication(final Context context, final AuthenticationDelegate delegate) {
         this.fingerprintCancellationSignal = new CancellationSignal();
-
+        Log.d(TAG, "Starting fingerprint authentication");
         FingerprintManagerCompat.AuthenticationCallback callback = new FingerprintManagerCompat.AuthenticationCallback() {
             @Override
             public void onAuthenticationError(int errMsgId, CharSequence errString) {
                 super.onAuthenticationError(errMsgId, errString);
-
+                Log.d(TAG, "onAuthenticationError: " + errMsgId + ", " + errString);
                 delegate.onResolutionRequired(R.string.applock__fingerprint_error_unknown);
             }
-
             @Override
             public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
                 super.onAuthenticationHelp(helpMsgId, helpString);
-
+                Log.d(TAG, "onAuthenticationHelp: " + helpMsgId + ", " + helpString);
                 delegate.onAuthenticationHelp(helpMsgId, helpString);
             }
-
             @Override
             public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
-
+                Log.d(TAG, "onAuthenticationSucceeded");
                 notifyEnrolled(context);
-
                 delegate.onAuthenticationSuccess(result);
             }
-
             @Override
             public void onAuthenticationFailed() {
                 super.onAuthenticationFailed();
-
+                Log.d(TAG, "onAuthenticationFailed");
                 delegate.onAuthenticationFailed(context.getString(R.string.applock__fingerprint_error_unrecognized));
             }
         };
-
         delegate.onAuthenticating(fingerprintCancellationSignal);
-
         try {
             Cipher cipher = generateAuthCipher(context, false, 0);
+            Log.d(TAG, "Cipher generated for fingerprint auth");
             FingerprintManagerCompat.CryptoObject cryptoObject = new FingerprintManagerCompat.CryptoObject(cipher);
-
             FingerprintManagerCompat manager = FingerprintManagerCompat.from(context);
             manager.authenticate(cryptoObject, 0, fingerprintCancellationSignal, callback, null);
+            Log.d(TAG, "manager.authenticate() called");
         }
         catch (Exception e) {
-            e.printStackTrace();
-
+            Log.e(TAG, "Exception during fingerprint authentication", e);
             delegate.onResolutionRequired(AppLock.ERROR_CODE_FINGERPRINTS_MISSING_HARDWARE);
         }
     }
