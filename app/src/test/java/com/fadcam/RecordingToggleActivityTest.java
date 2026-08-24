2bd09708dfb6edd7d9ba014639095855fdf1ac92
Trendyzima <momanyitrendy@gmail.com>
2026-08-24 21:01:34 +0300

    Fix stale recording toggle unit test

--- app/src/test/java/com/fadcam/RecordingToggleActivityTest.java
+++ app/src/test/java/com/fadcam/RecordingToggleActivityTest.java
@@ -1,22 +1,19 @@
 package com.fadcam;
 
 import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
-import org.mockito.InOrder;
 
 public class RecordingToggleActivityTest {
     @Test
-    public void finishWithoutUiBackgroundsTaskBeforeFinishing() {
+    public void finishWithoutUiFinishesActivityWithoutBackgroundingTask() {
         RecordingToggleActivity activity = mock(RecordingToggleActivity.class);
         doCallRealMethod().when(activity).finishWithoutUi();
 
         activity.finishWithoutUi();
 
-        InOrder completionOrder = inOrder(activity);
-        completionOrder.verify(activity).moveTaskToBack(true);
-        completionOrder.verify(activity).finish();
+        verify(activity).finish();
     }
 }
