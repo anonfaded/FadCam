ed5190382f4a67da5b3eae1e68da60a0550dafbf
Trendyzima <momanyitrendy@gmail.com>
2026-08-30 16:21:00 +0300

    chore(android): normalize production application id to lowercase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.Tv49east.com"
+        applicationId = "com.tv49east.com"
         minSdk = 24
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
